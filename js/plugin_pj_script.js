29a35f189d0f49dd97cce8280d70c558675cde5e
Paweł Jureczko <jureczko.pawel@gmail.com>
2021-05-22 22:38:17 +0200

    added js to done and delete buttons for each li element

--- js/plugin_pj_script.js
+++ js/plugin_pj_script.js
@@ -1,8 +1,22 @@
 const onlyForTest = document.querySelector('.todolist .todolist__list');
 const allTasks = onlyForTest.querySelectorAll('li');
+const todoButtons = document.querySelectorAll('.todolist__button');
 
 
-allTasks.forEach(item =>
-  item.addEventListener("click", function() {
-    console.log(item);
-  }));
+todoButtons.forEach(item => {
+
+  item.addEventListener("click", function () {
+    if (item.classList.contains('todolist__button--delete')) {
+      item.parentElement.parentElement.remove();
+    } else if (item.classList.contains("todolist__button--completed")) {
+      item.parentElement.parentElement.classList.toggle('completed');
+    }
+  })
+})
+
+// allTasks.forEach(item =>
+//   item.addEventListener("click", function() {
+//     console.log('hi');
+
+
+//   }));
