0a28ffbfde031401965620ae4c2943b636288ff4
Paweł Jureczko <jureczko.pawel@gmail.com>
2021-05-22 11:44:27 +0200

    initial setup for plugin , added security index php file

---
+++ pawel-jureczko-recruitment-task-plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @package pawel-jureczko-recruitment-task-plugin
+ */
+
+ /*
+  Plugin Name: Pawel Jureczko Recruitment Task Plugin
+  Plugin URI: https://github.com/PawelJureczko/recruitment-task-plugin
+  Description: ToDo List created as task for Web Wolf recruitment
+  Version: 1.0.0
+  Author: Pawel Jureczko
+  Author URI: https://www.linkedin.com/in/pawe%C5%82-jureczko-35a267191/
+  License: GPLv2 or later
+  */
+
