7a76b35617f26705f7efcd0ab61768a48c33db52
Paweł Jureczko <jureczko.pawel@gmail.com>
2021-05-22 17:04:12 +0200

    added proper import for css and js

---
+++ js/plugin_pj_script.js
@@ -0,0 +1,5 @@
+const onlyForTest = document.querySelector('#only-for-test');
+
+onlyForTest.addEventListener("click", function() {
+  console.log('test');
+})
--- pawel-jureczko-recruitment-task-plugin.php
+++ pawel-jureczko-recruitment-task-plugin.php
@@ -14,3 +14,49 @@
   License: GPLv2 or later
   */
 
+
+// prevents interference without permission
+  if ( ! defined( 'ABSPATH' ) ) {
+    die;
+  }
+
+//adding custom css for plugin
+function addThemeStyles() {
+  wp_enqueue_style('style_file' , plugin_dir_url(__FILE__).'/css/plugin_styles.css');
+}
+
+function todoPJ_enqueueBootstrapCSS()
+{
+    // CSS
+    wp_register_style('prefix_bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
+    wp_enqueue_style('prefix_bootstrap');
+}
+
+function addThemeScript() {
+
+  wp_enqueue_script('js-file', plugin_dir_url(__FILE__).'/js/plugin_pj_script.js', '', '1.0.0', true);
+}
+
+function mainToDoList() {
+  return '
+  <div class="another-test-file" id="only-for-test">
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <h1>Hello World!</h1>
+        </div>
+        <div class="col-6">
+          <h1>Another Hello World!</h1>
+          <button class="btn btn--testowo">asdasd</button>
+        </div>
+      </div>
+    </div>
+  </div>';
+}
+
+
+add_action('wp_enqueue_scripts', 'addThemeStyles');
+add_action('wp_enqueue_scripts', 'addThemeScript');
+add_action('wp_head', 'todoPJ_enqueueBootstrapCSS');
+
+add_shortcode('example', 'mainToDoList');
